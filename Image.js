1a310e817e53c0c401845babbbb95b81d65be825
alice souza <alicecsouza1@outlook.com>
2021-07-05 13:54:30 -0300

    frog primeiro commit

---
+++ Image.js
@@ -0,0 +1,42 @@
+//Imagens e Sons do jogo
+
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+//Sons 
+
+let somDaTrilha;
+let somDaColisao;
+let somDoPonto;
+
+function preload(){
+  imagemDaEstrada = loadImage("Image/estrada.png");
+  imagemDoAtor = loadImage("Image/ator-1.png");
+  imagemCarro = loadImage("Image/carro-1.png");
+  imagemCarro2 = loadImage("Image/carro-2.png")
+  imagemCarro3 = loadImage("Image/carro-3.png")
+  
+  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3]  
+  
+  somDaTrilha = loadSound ("Sons/trilha.mp3");
+  somDaColisao = loadSound ("Sons/colidiu.mp3");
+  somDoPonto = loadSound ("Sons/pontos.wav");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
